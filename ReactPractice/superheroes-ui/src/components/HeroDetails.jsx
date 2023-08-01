43285d787ff99e06166f303e70b3235bf89c07a7
i2nico <nicolas-mueller@gmx.net>
2023-08-01 10:02:49 +0100

    new images

--- ReactPractice/superheroes-ui/src/components/HeroDetails.jsx
+++ ReactPractice/superheroes-ui/src/components/HeroDetails.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
-import batman from '../images/batman.jpeg'
 import images from '../index.js'
 
 const HeroDetails = (props) => {
   return (
     <div>
-        <img src={images[props.info.alias.replaceAll(/\s/g,'')]} alt={props.info.name} height="250px"/>  
+        <img src={images[props.info.alias.replaceAll(/\s/g,'')]} alt={props.info.name} height="250px"/>
+        <p id="blue-alias">Alias: {props.info.alias}</p>
+        <p className="green-class">Name: {props.info.name}</p>
+        <button className="green-class">Like</button>  
     </div>
   )
 }
