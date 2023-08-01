4f22a642a53b936b41ba8292e7b4e751457eb0db
i2nico <nicolas-mueller@gmx.net>
2023-08-01 14:49:24 +0100

    QL8

--- ReactPractice/superheroes-ui/src/components/HeroForm.jsx
+++ ReactPractice/superheroes-ui/src/components/HeroForm.jsx
@@ -2,12 +2,15 @@ import React, { useState } from 'react'
 import Form from 'react-bootstrap/Form'
 import Row from 'react-bootstrap/Row';
 import Button from 'react-bootstrap/Button'
+import { saveHero } from '../services/hero-service';
+import { useNavigate } from 'react-router-dom';
 
 const HeroForm = () => {
     const [alias, setAlias] = useState('');
     const [name, setName] = useState('');
     const [ability, setAbility] = useState('');
     const [teamID, setTeamID] = useState(0);
+    const navigate = useNavigate();
 
     const handleAliasChange = (event) => {
         setAlias(event.target.value);
@@ -26,13 +29,24 @@ const HeroForm = () => {
     }
 
     const handleSubmit = (event) => {
+
         event.preventDefault();
         let hero = {};
         hero.alias = alias;
         hero.name = name;
-        hero.ability = ability;
+        hero.superpower = ability;
         hero.teamID = teamID;
-        console.log(hero)
+        saveHero(hero)
+        .then(res => {
+           setAbility('');
+           setAlias('');
+           setName('');
+           setTeamID(0)
+           navigate("/")
+           })
+         .catch(err=>{
+            console.log(err);
+           }) 
     } 
 
     return (
